fd3aa9f291259d909874fcddf708856c4eaca0a5
DataFox0 <datafox0@gmail.com>
2025-05-05 21:55:30 +0800

    。。。
    。。。

--- src/main/java/com/example/io/InputHandler.java
+++ src/main/java/com/example/io/InputHandler.java
@@ -1,33 +1,13 @@
 package com.example.io;
 
 import com.example.graph.*;
-
-import java.io.InputStream;
 import java.util.*;
 
 public class InputHandler {
     public static InputData readInput() {
         // Read attractions and road data
-        // Map<String, Attraction> attractions = CSVReader.readAttractions(
-        //     InputHandler.class.getClassLoader().getResource("attractions.csv").getPath()
-        // );
-        // Graph roadNetwork = CSVReader.readRoads(
-        //     InputHandler.class.getClassLoader().getResource("roads.csv").getPath()
-        // );
-
-        // 使用 getResourceAsStream 读取文件
-        InputStream attractionsStream = InputHandler.class.getClassLoader().getResourceAsStream("attractions.csv");
-        InputStream roadsStream = InputHandler.class.getClassLoader().getResourceAsStream("roads.csv");
-
-        if (attractionsStream == null || roadsStream == null) {
-            System.out.println("Files not found!");
-        }
-        // 使用 InputStream 处理文件内容
-        Map<String, Attraction> attractions = CSVReader.readAttractions(attractionsStream);
-        Graph roadNetwork = CSVReader.readRoads(roadsStream);
-        
-
-
+        Map<String, Attraction> attractions = CSVReader.readAttractions("resources/attractions.csv");
+        Graph roadNetwork = CSVReader.readRoads("resources/roads.csv");
 
         // Print the information of attraction
         System.out.println("Attractions:");
